473f6370f4c98a7760f1c03e676efccbf10fb67e
wafiir051-ai <wafiir051@smp.belajar.id>
2026-05-04 08:48:22 +0700

    feat: bigger wave divider

--- src/components/WaveDivider.jsx
+++ src/components/WaveDivider.jsx
@@ -1,21 +1,18 @@
-export default function WaveDivider({ fromColor = '#0a0a0a', toColor = '#ffffff', flip = false }) {
+export default function WaveDivider({ fromColor = '#0a0a0a', toColor = '#ffffff' }) {
   return (
     <div
       className="w-full overflow-hidden leading-none"
-      style={{ 
-        backgroundColor: fromColor,
-        marginBottom: '-2px'
-      }}
+      style={{ backgroundColor: fromColor, marginBottom: '-3px' }}
     >
       <svg
-        viewBox="0 0 1440 80"
+        viewBox="0 0 1440 160"
         xmlns="http://www.w3.org/2000/svg"
         preserveAspectRatio="none"
-        className="w-full h-16 md:h-20"
-        style={{ transform: flip ? 'scaleY(-1)' : 'none', display: 'block' }}
+        className="w-full h-32 md:h-48 lg:h-56"
+        style={{ display: 'block' }}
       >
         <path
-          d="M0,40 C180,80 360,0 540,40 C720,80 900,0 1080,40 C1260,80 1380,20 1440,40 L1440,80 L0,80 Z"
+          d="M0,60 C200,160 400,0 600,80 C800,160 1000,10 1200,80 C1320,120 1390,40 1440,70 L1440,160 L0,160 Z"
           fill={toColor}
         />
       </svg>
