7cd95503d2449a0a3ba6d9e71f864a77702c9e55
wafiir051-ai <wafiir051@smp.belajar.id>
2026-05-13 14:20:02 +0700

    feat: gelombang super meleleh kayak es krim

--- src/components/WaveDivider.jsx
+++ src/components/WaveDivider.jsx
@@ -2,13 +2,13 @@ export default function WaveDivider({ fromColor = '#0a0a0a', toColor = '#ffffff'
   return (
     <div style={{ backgroundColor: fromColor, lineHeight: 0, display: 'block' }}>
       <svg
-        viewBox="0 0 1440 120"
+        viewBox="0 0 1440 160"
         xmlns="http://www.w3.org/2000/svg"
         preserveAspectRatio="none"
-        style={{ display: 'block', width: '100%', height: '120px' }}
+        style={{ display: 'block', width: '100%', height: '160px' }}
       >
         <path
-          d="M0,60 Q120,10 240,50 T480,40 Q600,80 720,50 T960,60 Q1080,20 1200,55 T1440,50 L1440,120 L0,120 Z"
+          d="M0,80 Q60,20 120,60 Q180,100 240,50 Q300,0 360,70 Q420,140 480,60 Q540,30 600,80 Q660,130 720,70 Q780,10 840,90 Q900,150 960,80 Q1020,40 1080,100 Q1140,140 1200,70 Q1260,30 1320,90 Q1380,130 1440,80 L1440,160 L0,160 Z"
           fill={toColor}
         />
       </svg>
