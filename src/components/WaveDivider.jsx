60499789d7110a503a603d3b2991b00bb2183756
wafiir051-ai <wafiir051@smp.belajar.id>
2026-05-17 07:59:50 +0700

    fix: perbesar gelombang wave divider jadi ombak laut yang dramatis

--- src/components/WaveDivider.jsx
+++ src/components/WaveDivider.jsx
@@ -8,18 +8,18 @@ export default function WaveDivider({ fromColor = '#0a0a0a', toColor = '#ffffff'
       overflow: 'hidden'
     }}>
       <svg
-        viewBox="0 0 1440 120"
+        viewBox="0 0 1440 200"
         xmlns="http://www.w3.org/2000/svg"
         preserveAspectRatio="none"
         style={{ 
           display: 'block', 
           width: '100%', 
-          height: '80px',
+          height: '120px',
           transform: flip ? 'scaleY(-1)' : 'none'
         }}
       >
         <path
-          d="M0,60 C120,100 240,20 360,60 C480,100 600,20 720,60 C840,100 960,20 1080,60 C1200,100 1320,20 1440,60 L1440,120 L0,120 Z"
+          d="M0,100 C180,160 360,40 540,100 C720,160 900,40 1080,100 C1260,160 1380,40 1440,100 L1440,200 L0,200 Z"
           fill={toColor}
         />
       </svg>
